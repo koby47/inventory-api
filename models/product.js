ba7de48903e250c6830396c9621cf08b86a0bf5c
koby47 <samuelmensahquaye@gmail.com>
2025-03-07 14:54:22 +0000

    fourth

--- models/product.js
+++ models/product.js
@@ -3,13 +3,15 @@ import mongoose from "mongoose";
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   quantity: { type: Number,  },
-  price: { type: Number,  },
+  price: { type: Number,  required: true},
   supplier: { 
-    name: { type: String,  }, 
+    name: { type: String, required: true }, 
     contact: { type: String,  },
     email: { type: String }
   },
-  createdAt: { type: Date, default: Date.now }
+  image: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  // timestamps: true
 });
 
 export default mongoose.model("Product", ProductSchema);
